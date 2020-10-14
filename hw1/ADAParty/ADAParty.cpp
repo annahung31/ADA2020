bcee8db427c139e168e30bb199d5897d18fe5752
annahung31 <fbiannahung@gmail.com>
2020-10-14 18:49:57 +0800

    case 2 is god

--- hw1/ADAParty/ADAParty.cpp
+++ hw1/ADAParty/ADAParty.cpp
@@ -6,6 +6,23 @@
 #include <numeric>
 using namespace std;
 
+long long int max(long long int x, long long int y){
+    if (x > y){
+        return x;
+    }
+    else{
+        return y;
+    }
+};
+
+long long int min(long long int x, long long int y){
+    if (x > y){
+        return y;
+    }
+    else{
+        return x;
+    }
+};
 
 long long int findPair(vector<long long int> candies, long long int k){
     
@@ -40,6 +57,166 @@ long long int findPair(vector<long long int> candies, long long int k){
     return nPair;
     }
 
+void case1(vector<long long int>& candies, vector<long long int>& prefixSum, long long int& nPair, long long int& k, long long int st, long long int mid, long long int ed){
+    long long int l, r, i, MAX, MIN, ml, mr;
+    int flag=1;
+
+    MAX = max(candies[mid], candies[mid-1]);
+    MIN = min(candies[mid], candies[mid-1]);
+
+    //case 1 :  max min |
+    vector<long long int> Ml(k, 0), Mr(k, 0);
+
+    l=mid-1; r=mid+1;
+    while (l>=st){
+        MAX = max(MAX, candies[l]);
+        MIN = min(MIN, candies[l]);
+        cout << "Current MAX: " << MAX << ", MIN:" << MIN << "\n";
+
+        if (l==0){
+            ml = (0 + MAX + MIN) % k;
+        }
+        else{
+            ml = (prefixSum[l-1] + MAX + MIN) % k;
+        }
+        Ml[ml] +=1;
+        
+        if (flag==1){
+            if (MAX >= candies[r] && candies[r] >= MIN){
+                mr = prefixSum[r] % k;
+                Mr[mr] +=1;
+                
+                if (r <= ed-1){
+                    r++;
+                }
+                else{
+                    flag=0;
+                }
+            }
+            else{
+                l--;
+            }
+        }
+
+        else{
+            l--;
+        }
+        
+    }
+
+    for (i=0; i<k; i++){
+        nPair += Ml[i] * Mr[i];
+    }
+
+    return;
+
+}
+
+
+
+void case2(vector<long long int>& candies, vector<long long int>& prefixSum, long long int& nPair, long long int& k, long long int st, long long int mid, long long int ed){
+    long long int l, r, i, MAX, MIN, ml, mr;
+    int flag=1;
+
+    MAX = max(candies[mid+1], candies[mid+2]);
+    MIN = min(candies[mid+1], candies[mid+2]);
+
+    //case 2 :   | max min
+    vector<long long int> Ml(k, 0), Mr(k, 0);
+
+    l=mid; r=mid+2;
+    mr = (prefixSum[r] - MAX - MIN) % k;
+    Mr[mr] +=1;
+    while (r<=ed){
+        // cout << "r @ " << candies[r] << "\n";
+        if (flag==1){
+            // cout << "==see left: l @ " << candies[l] << "\n"; 
+            if (MAX >= candies[l] && candies[l] >= MIN){
+                // cout << "in the rage" << "\n";
+                if (l==0){
+                    ml = 0;
+                    flag = 0;
+                }
+                else{
+                    ml = prefixSum[l-1] % k;
+                }
+                Ml[ml] +=1;
+
+                if (l > 0){
+                    l--;
+                }
+                else{
+                    flag=0;
+                }
+                // cout << "l become " << l << "\n";
+            }
+            else{
+                // cout << "not in the range" << "\n"; 
+
+                if (r < ed){
+                    r++;
+                    
+                    MAX = max(MAX, candies[r]);
+                    MIN = min(MIN, candies[r]);
+                    // cout << "Current MAX: " << MAX << ", MIN:" << MIN << "\n";
+                    mr = (prefixSum[r] - MAX - MIN) % k;
+                    Mr[mr] +=1;
+                }
+                else{
+                    break;
+                }
+
+            }
+        }
+        
+        else{
+            if (r < ed){
+                r++;
+                
+                MAX = max(MAX, candies[r]);
+                MIN = min(MIN, candies[r]);
+                // cout << "Current MAX: " << MAX << ", MIN:" << MIN << "\n";
+                mr = (prefixSum[r] - MAX - MIN) % k;
+                Mr[mr] +=1;
+            }
+            else{
+                break;
+            }
+        }
+
+    }
+
+    for (i=0; i<k; i++){
+        // cout << i << " : " << Ml[i] << ", " << Mr[i] << "\n";
+        nPair += Ml[i] * Mr[i];
+    }
+
+    return;
+
+}
+
+
+
+void combine(vector<long long int>& candies, vector<long long int>& prefixSum, long long int& nPair, long long int& k, long long int st, long long int mid, long long int ed){
+    case1(candies, prefixSum, nPair, k, st, mid, ed);
+    case2(candies, prefixSum, nPair, k, st, mid, ed);
+}
+
+void getPair(vector<long long int>& candies, vector<long long int>& prefixSum, long long int& nPair, long long int& k, long long int st, long long int ed){
+    
+    if (ed-st < k){
+        return;
+    }
+    
+    long long int mid = (st+ed)/2;
+    getPair(candies, prefixSum, nPair, k, st, mid);
+    getPair(candies, prefixSum, nPair, k, mid+1, ed);
+
+    combine(candies, prefixSum, nPair, k, st, mid, ed);
+
+}
+
+
 
 int main(){ 
     
@@ -49,17 +226,33 @@ int main(){
     
     cin >> n >> k;          
 
-    vector<long long int> candies;
+    vector<long long int> candies, prefixSum;
     candies.assign(n, 0);  
-
+    prefixSum.assign(n, 0);
     for (i=0; i<n; i++){
             cin >> value;
             candies[i] = value;
-        
+            if (i==0){
+                prefixSum[i]=candies[i];
+            }
+            else{
+                prefixSum[i]=prefixSum[i-1] + candies[i];
+            }
     }
 
+    // for (i=0; i<n; i++){
+    //     cout << prefixSum[i] << " ";
+    // }
+    // cout << "\n";
+
+    // if (n<=200 && k <= 200){
+    //     nPair = findPair(candies, k);
+    // }
+    // else{
+    //     getPair(candies, prefixSum, nPair, k, 0, n-1);
+    // }
+    getPair(candies, prefixSum, nPair, k, 0, n-1);
     
-    nPair = findPair(candies, k);
     cout << nPair << endl;
     return 0; 
     }
