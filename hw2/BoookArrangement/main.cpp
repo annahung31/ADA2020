ac23f7ec5798667dcfd271f532aa9fa8492beb66
annahung31 <fbiannahung@gmail.com>
2020-11-02 20:01:43 +0800

    brute force

--- hw2/BoookArrangement/main.cpp
+++ hw2/BoookArrangement/main.cpp
@@ -4,26 +4,27 @@
 using namespace std;
 
 int books[100001];
-
+int tempbooks[100001];
 vector<int> output;
-
+vector<int> tempOut;
 
 void solu(int i, int j, int& insertNum){
-    cout << "cp: " << i << "," << j << endl;
-    if (books[i] == 0){
-        solu(j, j-1, insertNum);
-    }
-    if (books[j] == 0 && j > 0){
-        while (books[i] != 0){
-            output.push_back(i);
-            cout << i << endl;
-            books[i]--;
-            output.push_back(j);
-            cout << j << endl;
-            insertNum++;
-        }
-        solu(j-1, j-2, insertNum);
-    }
+    // cout << "cp: " << i << "," << j << endl;
+    // if (books[i] == 0){
+    //     solu(j, j-1, insertNum);
+    // }
+    // if (books[j] == 0 && j > 0){
+    //     while (books[i] != 0){
+    //         output.push_back(i);
+    //         cout << i << endl;
+    //         books[i]--;
+    //         output.push_back(j);
+    //         cout << j << endl;
+    //         insertNum++;
+    //     }
+    //     solu(j-1, j-2, insertNum);
+    // }
+
     if (i == 0 && j < 0){
         return;
     }
@@ -32,10 +33,10 @@ void solu(int i, int j, int& insertNum){
         books[i]--;
         while (books[i] != 0){
             output.push_back(i+1);
-            cout << i + 1 << endl;
+            // cout << i + 1 << endl;
             insertNum++;
             output.push_back(i);
-            cout << i << endl;
+            // cout << i << endl;
             books[i]--;
         }
         return;
@@ -44,32 +45,39 @@ void solu(int i, int j, int& insertNum){
     if (books[i] > books[j]){
         while (books[j] != 0){
             output.push_back(i);
-            cout << i << endl;
+            // cout << i << endl;
             books[i]--;
             output.push_back(j);
-            cout << j << endl;
+            //cout << j << endl;
             books[j]--;
         }
         while (books[i] != 0){
             output.push_back(i);
-            cout << i << endl;
+            //cout << i << endl;
             books[i]--;
-            output.push_back(j);
-            cout << j << endl;
-            insertNum++;
+
+            if(i == 2 && books[i] == 0){
+                return;
+            }
+            else{
+                output.push_back(j);
+                //cout << j << endl;
+                insertNum++;
+            }
+
         }
         solu(j-1, j-2, insertNum);
     }
     else{
         output.push_back(i);
-        cout << i << endl;
+        //cout << i << endl;
         books[i]--;
         while (books[i] != 0){
             output.push_back(j);
-            cout << j << endl;
+            //cout << j << endl;
             books[j]--;
             output.push_back(i);
-            cout << j << endl;
+            //cout << j << endl;
             books[i]--;
         }
         solu(j, j-1, insertNum);
@@ -82,7 +90,7 @@ void solu(int i, int j, int& insertNum){
 
 int main(){ 
     
-    int n, flag, i, value, insertNum = 0; 
+    int n, flag, i, value, insertNum = 0, tempInsertNum = 0; 
     
     cin >> n;          
 
@@ -90,26 +98,67 @@ int main(){
     for (i = 1; i <= n; i++){
             cin >> value;
             books[i] = value;
+            tempbooks[i] = value;
     }
     
     cin >> flag;
 
+    if (books[n] == 0){
+        solu(n-1, n-2, insertNum);
+    }
+    else{
+        solu(n, n-1, insertNum);
+    }
+    
+    tempInsertNum = insertNum;
+    insertNum = 0;
 
-    solu(n, n-1, insertNum);
-
-    // books[n-1]--;
-    // output.push_back(n-1);
-    // solu(n, n-1, insertNum);
-
-
-
-    cout << insertNum << "\n";
-    if (flag == 1){
-        for (i = 0; i < output.size(); i++){
-            cout << output[i] << " ";
-        }
-        cout << "\n";
+    for (i = 1; i <= n; i++){
+            books[i] = tempbooks[i];
     }
 
+    tempOut.assign(output.begin(), output.end());
+    output.clear();
+
+
+    if (books[n] == 0){
+        if (books[n-2] != 0){
+            books[n-2]--;
+            output.push_back(n-2);
+            solu(n-1, n-2, insertNum);
+        }
+        else{
+            solu(n-1, n-2, insertNum);
+        }
+
+    }
+    else{
+        books[n-1]--;
+        output.push_back(n-1);
+        solu(n, n-1, insertNum);
+    }
+
+
+    if (tempInsertNum > insertNum){
+        cout << insertNum << "\n";
+        if (flag == 1){
+            for (i = 0; i < output.size(); i++){
+                cout << output[i] << " ";
+            }
+            cout << "\n";
+        }
+    }
+    else{
+        cout << tempInsertNum << "\n";
+        if (flag == 1){
+            for (i = 0; i < tempOut.size(); i++){
+                cout << tempOut[i] << " ";
+            }
+            cout << "\n";
+        }
+    }
+
+
+    // solu(n-1, n-2, insertNum);
     return 0; 
     }
