6ced01216c20508cdc48620e292d30907813ed32
annahung31 <fbiannahung@gmail.com>
2020-11-05 11:44:41 +0800

    temp

--- hw2/BoookArrangement/main.cpp
+++ hw2/BoookArrangement/main.cpp
@@ -5,93 +5,94 @@ using namespace std;
 
 int books[100001];
 int tempbooks[100001];
-vector<int> output;
-vector<int> tempOut;
 
-void solu(int i, int j, int& insertNum){
-    // cout << "cp: " << i << "," << j << endl;
-    // if (books[i] == 0){
-    //     solu(j, j-1, insertNum);
-    // }
-    // if (books[j] == 0 && j > 0){
-    //     while (books[i] != 0){
-    //         output.push_back(i);
-    //         cout << i << endl;
-    //         books[i]--;
-    //         output.push_back(j);
-    //         cout << j << endl;
-    //         insertNum++;
-    //     }
-    //     solu(j-1, j-2, insertNum);
-    // }
 
-    if (i == 0 && j < 0){
+void solu(int i, int j, int& ed, int& insertNum, vector< vector<int>>& output){
+    // cout << "check i:" << i << "," << books[i] << ", j:" << j << "," << books[j] << "\n";
+    if (i < ed && j < ed){
         return;
     }
-    if(i == 1 && j == 0){
-        output.push_back(i);
+    if(i == ed && j == ed-1){
+        output.push_back({i, -1, 1});
         books[i]--;
-        while (books[i] != 0){
-            output.push_back(i+1);
-            // cout << i + 1 << endl;
-            insertNum++;
-            output.push_back(i);
-            // cout << i << endl;
-            books[i]--;
-        }
+        insertNum += books[i];
+        output.push_back({i+1, i, books[i]});
+        books[i] = 0;
         return;
     }
-    
+
+    // if (books[i] == 0 && books[j] == 0){
+    //     insertNum += 2;
+    //     output.push_back({i, j, 1});
+    //     solu(j-1, j-2, ed, insertNum, output);
+    // }
+
+
     if (books[i] > books[j]){
-        while (books[j] != 0){
-            output.push_back(i);
-            // cout << i << endl;
-            books[i]--;
-            output.push_back(j);
-            //cout << j << endl;
-            books[j]--;
-        }
-        while (books[i] != 0){
-            output.push_back(i);
-            //cout << i << endl;
-            books[i]--;
+        output.push_back({i, j, books[j]});
+        books[i] = books[i] - books[j];
+        books[j] = 0;
 
-            if(i == 2 && books[i] == 0){
-                return;
-            }
-            else{
-                output.push_back(j);
-                //cout << j << endl;
-                insertNum++;
-            }
-
+        if (i == ed+1){
+            insertNum = insertNum + books[i] -1;
+            output.push_back({i, j, books[i]-1});
+            output.push_back({i, -1, 1});
+            books[i] = 0;
         }
-        solu(j-1, j-2, insertNum);
+        else{
+            insertNum += books[i];
+            output.push_back({i, j, books[i]});
+            books[i] = 0;
+        }
+
+        solu(j-1, j-2, ed, insertNum, output);
     }
     else{
-        output.push_back(i);
-        //cout << i << endl;
-        books[i]--;
-        while (books[i] != 0){
-            output.push_back(j);
-            //cout << j << endl;
-            books[j]--;
-            output.push_back(i);
-            //cout << j << endl;
+        if (books[i] != 0){
+            output.push_back({i, -1, 1});
+            //cout << i << endl;
             books[i]--;
+            output.push_back({j, i, books[i]});
+            books[j] = books[j] - books[i];
+            books[i] = 0;
+
         }
-        solu(j, j-1, insertNum);
+        else{
+            insertNum++;
+            output.push_back({i, -1, 1});
+            
+        }
+        solu(j, j-1, ed, insertNum, output);
+
     }
 
 
 }  
 
 
+void printSeq(vector< vector<int>>& output){
+    int i, j;
+    for (i = 0; i < output.size(); i++){
+        if (output[i][1] == -1){
+            cout << output[i][0] << " ";
+        }
+        else{
+            for (j = 0; j < output[i][2]; j++){
+                cout << output[i][0] << " ";
+                cout << output[i][1] << " ";
+            }
+        }
+        
+    }
+    cout << "\n";
+}
+
 
 int main(){ 
     
-    int n, flag, i, value, insertNum = 0, tempInsertNum = 0; 
-    
+    int n, flag, i, value, ed, st, insertNum = 0, InsertNumA = 0; 
+    vector< vector<int>> outputA;
+    vector< vector<int>> outputB;
     cin >> n;          
 
 
@@ -103,62 +104,61 @@ int main(){
     
     cin >> flag;
 
-    if (books[n] == 0){
-        solu(n-1, n-2, insertNum);
+
+    //remove front zeros and back zeros
+    for (i = 1; i <= n; i++){
+        ed = i;
+        if (books[i] != 0) break;
     }
-    else{
-        solu(n, n-1, insertNum);
+
+    for (i = n; i >= 1; i--){
+        st = i;
+        if (books[i] != 0) break;
     }
-    
-    tempInsertNum = insertNum;
+  
+
+
+
+    solu(st, st-1, ed, insertNum, outputA);
+
+
+    InsertNumA = insertNum;
     insertNum = 0;
 
     for (i = 1; i <= n; i++){
             books[i] = tempbooks[i];
     }
 
-    tempOut.assign(output.begin(), output.end());
-    output.clear();
 
 
-    if (books[n] == 0){
-        if (books[n-2] != 0){
-            books[n-2]--;
-            output.push_back(n-2);
-            solu(n-1, n-2, insertNum);
-        }
-        else{
-            solu(n-1, n-2, insertNum);
-        }
-
+    //case 2
+    if (books[st-1] != 0){
+        books[st-1]--;
+        outputB.push_back({st-1, -1, 1});
+        solu(st, st-1, ed, insertNum, outputB);
     }
     else{
-        books[n-1]--;
-        output.push_back(n-1);
-        solu(n, n-1, insertNum);
+        // solu(st, st-1, ed, insertNum, outputB);
+        insertNum = InsertNumA;
+
     }
 
 
-    if (tempInsertNum > insertNum){
+
+    if (InsertNumA > insertNum){
         cout << insertNum << "\n";
         if (flag == 1){
-            for (i = 0; i < output.size(); i++){
-                cout << output[i] << " ";
-            }
-            cout << "\n";
+            printSeq(outputB);
         }
     }
     else{
-        cout << tempInsertNum << "\n";
+        cout << InsertNumA << "\n";
         if (flag == 1){
-            for (i = 0; i < tempOut.size(); i++){
-                cout << tempOut[i] << " ";
-            }
-            cout << "\n";
+            printSeq(outputA);
         }
     }
 
 
-    // solu(n-1, n-2, insertNum);
     return 0; 
     }
+
